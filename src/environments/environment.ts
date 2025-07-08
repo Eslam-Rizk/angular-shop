2531f12ced2f92e2bba863b47d96d3f668054f53
eslam-rizk <eslam.mahmoud18@yahoo.com>
2025-07-08 22:34:53 +0300

    added new github clientId and secret

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,9 @@
 export const environment = {
   production: false,
-  githubClientId: "Ov23ligMdIjY65ySpzOA",
-  githubClientSecret: "85f9e88d03a046987ddc7286bf78d8acf62d54cd",
+  // githubClientId: "Ov23ligMdIjY65ySpzOA",
+  githubClientId: "Ov23lirZfBRxeYNdfnxr",
+  // githubClientSecret: "85f9e88d03a046987ddc7286bf78d8acf62d54cd",
+  githubClientSecret: "a05df77fc2d081ead2db232b971d97c73794b75d",
   // githubRedirectUri: "http://localhost:4200/login",
   githubRedirectUri:
     "https://angular-shop-hc9o40oxi-eslam-rizks-projects.vercel.app/login",
