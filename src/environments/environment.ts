131b339fd4fc3110e37865c018ce82b06335e6a9
eslam-rizk <eslam.mahmoud18@yahoo.com>
2025-07-08 22:30:42 +0300

    added hosted redirect url

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,5 +2,7 @@ export const environment = {
   production: false,
   githubClientId: "Ov23ligMdIjY65ySpzOA",
   githubClientSecret: "85f9e88d03a046987ddc7286bf78d8acf62d54cd",
-  githubRedirectUri: "http://localhost:4200/login",
+  // githubRedirectUri: "http://localhost:4200/login",
+  githubRedirectUri:
+    "https://angular-shop-hc9o40oxi-eslam-rizks-projects.vercel.app/login",
 };
